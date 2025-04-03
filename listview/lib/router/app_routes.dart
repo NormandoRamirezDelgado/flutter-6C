220dfeed7ef36873d3c4a3d926e5f6f19bfa74f2
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2025-04-03 11:19:21 -0600

    Rutas Actualizado

--- listview/lib/router/app_routes.dart
+++ listview/lib/router/app_routes.dart
@@ -26,14 +26,6 @@ class AppRoutes {
     return appRoutes;
   }
 
-  // static Map<String, Widget Function(BuildContext)> routes = {
-  //   'home'      : (BuildContext context) => HomeScreen(),
-  //   'listview1' : (BuildContext context) => ListView1Screen(),
-  //   'listview2' : (BuildContext context) => ListView2Screen(),
-  //   'card'      : (BuildContext context) => CardScreen(),
-  //   'alert'     : (BuildContext context) => AlertScreen(),
-  // };
-
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     return MaterialPageRoute(
       builder: (context) => AlertScreen(),
